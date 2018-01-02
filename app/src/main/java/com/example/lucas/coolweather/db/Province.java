3041c3d5814de01d33a5a8d9903d4d87cb6714f7
Lucas <59329596@qq.com>
2018-01-02 15:06:19 +0800

    2

---
+++ app/src/main/java/com/example/lucas/coolweather/db/Province.java
@@ -0,0 +1,40 @@
+package com.example.lucas.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by lucas on 2018/1/2.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
